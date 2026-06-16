a0f989d0eabd6fccae6988b67ee481972ac720d3
tidraft <tidraft@users.noreply.github.com>
2026-06-16 19:00:38 +0800

    修复部署路径和预加载设置

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+  base: './',
   plugins: [react(), viteSingleFile()],
   build: {
     outDir: 'docs',
     target: 'es2017',
     cssCodeSplit: false,
     assetsInlineLimit: 100000000,
+    modulePreload: false,
   },
 });
